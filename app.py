0a9bb3f19eb7622028de3165be7c12ce465f332c
KatherinBK <k93664875@gmail.com>
2025-04-15 12:26:05 -0500

    fix:identacion de chistes

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import json
 
 app = Flask(__name__)
 #Arreglo que contiene los chistes
-chistes ={ [
+chistes = {
     "mujeres": [
-      {"id":1, "titulo":"el chiste","chiste":"un chiste"}
-      {"id":1, "titulo":"el chiste","chiste":"un chiste"}]
+        {"id": 1, "titulo": "el chiste", "chiste": "un chiste"},
+        {"id": 2, "titulo": "otro chiste", "chiste": "otro chiste"}
+    ],
     "gordos": [
-        {"id":1, "titulo":"el chiste","chiste":"un chiste"}
-        {"id":1, "titulo":"el chiste","chiste":"un chiste"}]
-]
+        {"id": 1, "titulo": "el chiste", "chiste": "un chiste"},
+        {"id": 2, "titulo": "otro chiste", "chiste": "otro chiste"}
+    ]
 }
+
 @app.route('/', methods =['GET'])
 def get_books():
     #funcion para devolvder el contenido de "chistes" en formato json
